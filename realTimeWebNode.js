2c50496270a61c172c8ab267981b77feda0dea90
Katarzyna Polaszk <polaszkk@gmail.com>
2015-10-30 15:07:26 +0100

    Update realTimeWebNode.js

--- realTimeWebNode.js
+++ realTimeWebNode.js
@@ -439,4 +439,121 @@ server.on('answer', function(question, answer) {
   answerQuestion(question, answer);
 });
 
-//
+// 7.2 Simple Redis Commands
+var redis = require('redis');
+var client = redis.createClient();
+client.set('name', 'John');
+
+// 7.3 Get A Key 
+var redis = require('redis');
+var client = redis.createClient();
+
+client.get('question', function(error, data) {
+  console.log(data);
+});
+
+// 7.4 Working With Lists 1
+var redis = require('redis');
+var client = redis.createClient();
+
+var question1 = "Where is the dog?";
+var question2 = "Where is the cat?";
+
+client.lpush('questions', question1, function(error, value) {
+  console.log(value);
+});
+
+client.lpush('questions', question2, function(error, value) {
+  console.log(value);
+});
+
+// 7.5 Working With Lists 2
+var redis = require('redis');
+var client = redis.createClient();
+      
+client.lrange('questions', 0, -1, function(error, value) {
+  console.log(value);
+});
+
+// 7.6 Persisting Questions
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var socket = require('socket.io');
+var io = socket.listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      redisClient.lpush('questions', question);
+    }
+  });
+});
+
+// 7.7 Emitting Stored Questions
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  redisClient.lrange('questions', 0, -1, function(error, value) {
+    value.forEach(function(question) {
+      client.emit('question', question);
+    });
+  });
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      redisClient.lpush("questions", question);
+    }
+  });
+});
+
+// 7.8 Limiting Questions Stored 
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  redisClient.lrange("questions", 0, -1, function(err, questions) {
+    questions.forEach(function(question) {
+      client.emit("question", question);
+    });
+  });
+
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      redisClient.lpush('questions', question, function() {
+        redisClient.ltrim('questions', 0, 19);
+      });
+    }
+  }); 
+});
