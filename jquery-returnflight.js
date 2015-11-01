42739d5a3692ee8ba795db8851d1dbf5e17b34cc
Katarzyna Polaszk <polaszkk@gmail.com>
2015-11-01 18:59:42 +0100

    Update jquery-returnflight.js

--- jquery-returnflight.js
+++ jquery-returnflight.js
@@ -489,7 +489,136 @@ $(document).ready(function() {
   });
 });
 
-// 5.7 - 5.13
+// 5.8 Declaring a jQuery plugin
+$.fn.photofy = function(){
+	console.log(this);
+}
+
+$(document).ready(function() {
+	$('.tour').photofy();
+});
+
+// 5.9 Iteration with each
+$.fn.photofy = function() {
+  this.each(function(){
+  	console.log(this);
+  });
+}
+
+$(document).ready(function() {
+  $('.tour').photofy();
+});
+
+// 5.10 Plugin Event
+$.fn.photofy = function() {
+  this.each(function() {
+ 		var tour = $(this);
+    tour.on('click.photofy', '.see-photos', function(event) {
+      event.preventDefault();
+    	tour.addClass('is-showing-photofy');
+    });
+  });
+}
+
+$(document).ready(function() {
+  $('.tour').photofy();
+});
+
+// 5.11 Using $.extend()
+$.fn.photofy = function(options) {
+  this.each(function() {
+    var show = function(e) {
+       e.preventDefault();
+       settings.tour
+               .addClass('is-showing-photofy')
+               .find('.photos')
+               .find('li:gt('+(settings.count-1)+')').hide();
+     }
+     var settings = $.extend({
+       count: 3,
+       tour: $(this)
+     }, options);
+     settings.tour.on('click.photofy', '.see-photos', show);
+  });
+}
+
+$(document).ready(function() {
+  $('.tour').photofy({ count: 1});
+});
+
+// 5.12 External Triggers
+$.fn.photofy = function(options) {
+  this.each(function() {
+    var show = function(e) {
+      e.preventDefault();
+      settings.tour
+              .addClass('is-showing-photofy')
+              .find('.photos')
+              .find('li:gt('+(settings.photos-1)+')')
+              .hide();
+    };
+    var settings = $.extend({
+      count: 3,
+      tour: $(this)
+    }, options);
+    settings.tour.on('click.photofy', '.see-photos', show);
+    settings.tour.on('show.photofy', show);
+  });
+}
+
+$(document).ready(function() {
+  $('.tour').photofy({ count: 1});
+
+  $('.show-photos').on('click', function(e) {
+    e.preventDefault();
+    $('.tour').trigger('show.photofy');
+  });
+});
+
+// 5.13 Internal Triggers
+$.fn.photofy = function(options) {
+  this.each(function() {
+    var show = function(e) {
+      e.preventDefault();
+      settings.tour
+              .addClass('is-showing-photofy')
+              .find('.photos')
+              .find('li:gt('+(settings.count-1)+')')
+              .hide();
+    }
+    var settings = $.extend({
+      count: 3,
+      tour: $(this)
+    }, options);
+    var remove = function(e) {
+      e.preventDefault();
+      settings.tour.fadeOut().off('.photofy');
+    }
+    settings.tour.on('click.photofy', '.see-photos', show);
+    settings.tour.on('show.photofy', show);
+    settings.tour.on('click.photofy', '.hide-tour', remove);
+  });
+}
+
+$(document).ready(function() {
+  $('.tour').photofy({ count: 1});
+
+  $('.show-photos').on('click', function(e) {
+    e.preventDefault();
+    $('.tour').trigger('show.photofy');
+  });
+});
+
+// 6.3 Reusable AJAX
+var Vacation = {
+  getPrice: function(location) {
+    var promise = $.ajax('/vacation/prices', {
+      data: {q: location}
+    });
+
+    return promise;
+  }
+};
 
 // 6.3 Reusable AJAX
 var Vacation = {
